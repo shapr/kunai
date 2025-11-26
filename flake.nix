2253949b5a27d4b44542613ba5ec8d9e35da6636
Shae Erisson <shae@scannedinavian.com>
2025-11-26 09:56:49 -0500

    started, still needs libs for pkg-config

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "kunai is a command line bloom filter calculator that outputs colors to an RGB matrix";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    rust-overlay = { url = "github:oxalica/rust-overlay"; };
+  };
+  outputs = { nixpkgs, rust-overlay, ... }:
+    let
+      system = "x86_64-linux";
+    in {
+      packages.${system}.default =
+        let pkgs = import nixpkgs { inherit system; };
+        in pkgs.rustPlatform.buildRustPackage {
+          pname = "kunai";
+          nativeBuildInputs = [ pkgs.pkg-config ];
+          buildInputs = [ pkgs.libudev-zero ];
+          version = "0.1.0";
+          cargoLock.lockFile = ./Cargo.lock;
+          src = pkgs.lib.cleanSource ./.;
+
+        };
+      devShells.${system}.default =
+        let pkgs = import nixpkgs {
+              inherit system;
+              overlays = [ (import rust-overlay) ];
+              config.allowUnfree = true;
+            };
+        in
+          pkgs.mkShell {
+            libraries = with pkgs; [ libudev-zero ];
+            packages = with pkgs; [ rust-bin.stable.latest.default rust-analyzer cargo libudev-zero pkg-config ];
+            shellHook = ''
+            export PKG_CONFIG_PATH=${pkgs.lib.concatStrings ["${pkgs.libudev-zero}" "/lib/pkgconfig"]}
+            '';
+          };
+    };
+}
